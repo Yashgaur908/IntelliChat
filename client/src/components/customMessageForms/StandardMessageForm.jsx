801bdb80d023bf9e0c21dc44d4d0a3d3630af44c
Yashgaur908 <Yashgaur908@gmail.com>
2023-06-15 23:11:39 +0530

    added

---
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { XMarkIcon } from "@heroicons/react/24/solid"
+
+const StandardMessageForm = () => {
+    const [message, setMessage] = useState("");
+    const [attachment, setAttachment] = useState("");
+    const [preview, setPreview] = useState("");
+    return (
+        <div className='message-form-container'>
+            {preview && (
+                <div className='message-form-preview'>
+                    <img alt='message-form-preview' className='message-form-preview-image' src={preview} onLoad={() => URL.revokeObjectURL(preview)} />
+                    <XMarkIcon className='message-form-icon-x' onClick={ ()=> {
+                            setPreview("") 
+                            setAttachment("")
+                        }}
+                    />
+                </div>
+            )}  
+        </div>
+    )
+}
+
+export default StandardMessageForm
